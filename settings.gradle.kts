1ad20cc4ab8aeea587bd14aaeaf2751a4daaad22
Niklas Widmann <nik@somethingcatchy.net>
2026-05-21 19:21:22 +0200

    update gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,10 @@
-val mod_name: String by extra
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven { url = uri("https://maven.minecraftforge.net/") }
-        maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
+        mavenLocal()
     }
 }
 
-rootProject.name = mod_name
+plugins {
+    id("com.possible-triangle.helper") version ("1.4")
+}
